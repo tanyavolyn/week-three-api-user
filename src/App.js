1ac73df5f347e838ee4a05ca68942041bf2332e7
tanyavolyn <tanyavolyn@gmail.com>
2022-12-18 14:55:19 +0100

    your massage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+person: null
+  }
+
+  async componentDidMount(){
+    const url = "https://api.randomuser.me/";
+    const responce = await fetch(url);
+    const data = await responce.json();
+   this.setState({person: data.results[0]});
+
+
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!this.state.person ? <p>Loading...</p> :  
+      <div>
+           <img src={this.state.person.picture.large} alt="bild"/>
+      <p>{this.state.person.name.first} {this.state.person.name.last}</p>
+  
+      <p>{this.state.person.location.city}, {this.state.person.location.country}</p>
+    
+      </div>
+      }
+   
+     
+   
+ 
     </div>
   );
-}
+}}
 
 export default App;
